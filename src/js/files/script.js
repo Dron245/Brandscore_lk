0af24a22116927d03df8f9a40bb04e6ec621763e
Dron245 <Dron245@yandex.ru>
2023-11-05 12:05:13 +0300

    0511-1205

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,22 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+	const targetElement = e.target;
+	if (targetElement.closest('.top__user')) {
+		document.querySelector('.top__drop-menu').classList.add('topmenu-open')
+	} else {
+		// document.querySelector('.top__drop-menu').classList.remove('topmenu-open')
+		document.querySelector('.top__drop-menu') ? document.querySelector('.top__drop-menu').classList.remove('topmenu-open') : null
+	}
+	
+	if (targetElement.closest('.main-bottom__button_menu')) {
+		document.querySelector('.main-bottom__sublist').classList.add('exportmenu-open')
+	} else {
+		// document.querySelector('.main-bottom__sublist').classList.remove('exportmenu-open')
+		document.querySelector('.main-bottom__sublist') ? document.querySelector('.main-bottom__sublist').classList.remove('exportmenu-open') : null
+	}
+}
