39ad11bb052e71455f05385be7c7a02b33d39771
Dron245 <Dron245@yandex.ru>
2023-11-06 14:32:54 +0300

    0611-1432

--- src/js/files/forms/datepicker.js
+++ src/js/files/forms/datepicker.js
@@ -7,8 +7,8 @@ import { flsModules } from "../modules.js";
 // Подключение модуля
 import datepicker from 'js-datepicker';
 
-if (document.querySelector('[data-datepicker]')) {
-	const picker = datepicker('[data-datepicker]', {
+if (document.querySelector('.bottom-main__input')) {
+	const picker1 = datepicker('[data-datepicker]', {
 		customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
 		customMonths: ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сент", "Окт", "Нояб", "Дек"],
 		overlayButton: 'Применить',
@@ -22,5 +22,41 @@ if (document.querySelector('[data-datepicker]')) {
 
 		}
 	});
-	flsModules.datepicker = picker;
+	flsModules.datepicker = picker1;
+}
+//  var datepicker = new Datepicker('#datepicker');
+if (document.querySelector('.resources__input')) {
+	const picker2 = datepicker('[data-datepicker2]', {
+		customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
+		customMonths: ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сент", "Окт", "Нояб", "Дек"],
+		overlayButton: 'Применить',
+		overlayPlaceholder: 'Год (4 цифры)',
+		startDay: 1,
+		formatter: (input, date, instance) => {
+			const value = date.toLocaleDateString()
+			input.value = value
+		},
+		onSelect: function (input, instance, date) {
+
+		}
+	});
+	flsModules.datepicker = picker2;
+}
+
+if (document.querySelector('.resources__input_pl')) {
+	const picker3 = datepicker('[data-datepicker3]', {
+		customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
+		customMonths: ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сент", "Окт", "Нояб", "Дек"],
+		overlayButton: 'Применить',
+		overlayPlaceholder: 'Год (4 цифры)',
+		startDay: 1,
+		formatter: (input, date, instance) => {
+			const value = date.toLocaleDateString()
+			input.value = value
+		},
+		onSelect: function (input, instance, date) {
+
+		}
+	});
+	flsModules.datepicker = picker3;
 }
